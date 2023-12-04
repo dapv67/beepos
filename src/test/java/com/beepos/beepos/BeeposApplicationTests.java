fb0f36f9667dcfdc1bff56f68de706a7ea8ffbc0
dapv67 <alvarodperezv@gmail.com>
2023-12-04 17:36:50 -0600

    initial structure project

---
+++ src/test/java/com/beepos/beepos/BeeposApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beepos.beepos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeeposApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
